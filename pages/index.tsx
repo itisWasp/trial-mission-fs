d520a32ca67954bd5db56289832cd15e96506ccc
itisWasp <mugishaisrael18@gmail.com>
2022-05-31 14:49:31 +0200

    Added new changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
